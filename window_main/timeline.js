12992cd2be84bdc8f789b1cb125efdd5e1e4f0b0
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2019-09-23 08:19:11 -0300

    update

--- window_main/timeline.js
+++ window_main/timeline.js
@@ -1,10 +1,13 @@
 /*
 global
   am4core,
-  am4charts
+  am4charts,
+  am4themes_dark,
+  am4themes_animated
 */
 //const db = require("../shared/database");
 const playerData = require("../shared/player-data");
+const _ = require("lodash");
 
 const { hideLoadingBars, resetMainContainer } = require("./renderer-util");
 const { get_rank_index } = require("../shared/util");
@@ -42,7 +45,46 @@ function openTimelineTab() {
   let seasonType = "constructed";
   let chartDiv = createDiv(["chartdiv"]);
 
+  let season = playerData.rank.constructed.seasonOrdinal;
+  let chartData = playerData.seasonalRank(season, seasonType);
+  chartData = chartData.map(obj => {
+    let yPos = getRankY(obj.newClass, obj.newLevel, obj.newStep);
+    obj.date = new Date(obj.timestamp);
+
+    let match = playerData[obj.lastMatchId];
+    obj.deck = false;
+    obj.deckId = match.playerDeck.id;
+    obj.deckName = match.playerDeck.name;
+    obj.deckColors = match.playerDeck.colors;
+
+    if (obj.oldLevel !== obj.newLevel) {
+      obj.bullet = `../images/rank_${seasonType.toLowerCase()}/${
+        obj.newClass
+      }_${obj.newLevel}.png`;
+    } else {
+      obj.bullet = "";
+    }
+    return { ...obj, yPos };
+  });
+
+  am4core.useTheme(am4themes_dark);
+  am4core.useTheme(am4themes_animated);
   var chart = am4core.create(chartDiv, am4charts.XYChart);
+
+  // Give each deck a new color
+  let deckIds = Object.keys(_.groupBy(chartData, "deckId"));
+  let deckColors = {};
+  deckIds.forEach(id => {
+    deckColors[id] = chart.colors.next();
+    chart.colors.next();
+  });
+
+  chartData = chartData.map(obj => {
+    let color = deckColors[obj.deckId];
+    return { ...obj, color };
+  });
+
+  chart.data = chartData;
 
   // Create axes
   var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
@@ -52,15 +94,15 @@ function openTimelineTab() {
 
   // Create series
   var series = chart.series.push(new am4charts.LineSeries());
+  series.tooltipText = "{deckName}";
+  series.tooltip.pointerOrientation = "vertical";
   series.dataFields.valueY = "yPos";
   series.dataFields.dateX = "date";
-  series.strokeWidth = 2;
-  series.minBulletDistance = 10;
-  series.tooltipText = "{yPos}";
-  series.tooltip.pointerOrientation = "vertical";
-  series.tooltip.background.cornerRadius = 20;
-  series.tooltip.background.fillOpacity = 0.5;
-  series.tooltip.label.padding(12, 12, 12, 12);
+  series.strokeWidth = 3;
+  series.fillOpacity = 0.5;
+  series.minBulletDistance = 1;
+  series.propertyFields.stroke = "color";
+  series.propertyFields.fill = "color";
 
   // Add scrollbar
   chart.scrollbarX = new am4charts.XYChartScrollbar();
@@ -70,33 +112,6 @@ function openTimelineTab() {
   chart.cursor = new am4charts.XYCursor();
   chart.cursor.xAxis = dateAxis;
   chart.cursor.snapToSeries = series;
-
-  // Add data
-  let season = playerData.rank.constructed.seasonOrdinal;
-  chart.data = playerData.seasonalRank(season, seasonType);
-  chart.data = chart.data.map(obj => {
-    let yPos = getRankY(obj.newClass, obj.newLevel, obj.newStep);
-    obj.date = new Date(obj.timestamp);
-    /*
-    obj.bullet = series.bullets.push(new am4charts.Bullet());
-    if (obj.newLevel !== obj.oldLevel) {
-      let image = obj.bullet.createChild(am4core.Image);
-      image.href = `../images/rank_${seasonType}/${obj.newClass}_${
-        obj.newStep
-      }.png`;
-      image.minWidth = 120;
-      image.outerHeight = 24;
-      image.width = 24;
-      image.height = 24;
-      console.log(image.element);
-      image.horizontalCenter = "middle";
-      image.verticalCenter = "middle";
-    }
-    */
-    return { ...obj, yPos };
-  });
-
-  console.log(chart.data);
 
   mainDiv.appendChild(chartDiv);
 }
