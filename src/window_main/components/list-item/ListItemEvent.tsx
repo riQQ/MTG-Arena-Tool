1afa577e954c2cecc7c87743326980d09e4af768
Benjamin Lu <lusbenjamin@users.noreply.github.com>
2020-03-16 13:35:04 -0700

    optimize ListItemEvent renders (#932)

--- src/window_main/components/list-item/ListItemEvent.tsx
+++ src/window_main/components/list-item/ListItemEvent.tsx
@@ -29,25 +29,7 @@ import {
 import { InternalMatch } from "../../../types/match";
 import uxMove from "../../uxMove";
 
-export function ListItemEvent({
-  row
-}: TableViewRowProps<EventTableData>): JSX.Element {
-  const event = row.original;
-  const dispatcher = useDispatch();
-  const [expanded, setExpanded] = React.useState(false);
-
-  const onRowClick = (): void => {
-    setExpanded(!expanded);
-  };
-
-  const [hover, setHover] = React.useState(false);
-  const mouseEnter = React.useCallback(() => {
-    setHover(true);
-  }, []);
-  const mouseLeave = React.useCallback(() => {
-    setHover(false);
-  }, []);
-
+function DraftRares({ event }: { event: EventTableData }): JSX.Element {
   const draftId = event.id + "-draft";
   let draftRares: JSX.Element[] = [];
   if (pd.draftExists(draftId)) {
@@ -64,20 +46,107 @@ export function ListItemEvent({
         })
     ];
   }
+  return (
+    <div
+      style={{ flexGrow: 1, display: "flex", justifyContent: "center" }}
+      className="list_item_center"
+    >
+      {draftRares}
+    </div>
+  );
+}
 
-  const matchRows: InternalMatch[] = event.stats.matchIds
-    .map(pd.match)
-    .filter(match => match !== undefined) as InternalMatch[];
-  matchRows.sort((a, b) => {
-    if (!a || !b) return 0;
-    return compareDesc(new Date(a.date), new Date(b.date));
-  });
-  if (pd.draftExists(draftId)) {
-    matchRows.unshift(pd.draft(draftId));
-  }
+function EventMainRow({
+  event,
+  onRowClick
+}: {
+  event: EventTableData;
+  onRowClick: () => void;
+}): JSX.Element {
+  const eventName =
+    db.events[event.InternalEventName] ??
+    event.displayName ??
+    event.InternalEventName ??
+    "Unknown";
+  const eventColors = event.colors ?? [];
+
+  const [hover, setHover] = React.useState(false);
+  const mouseEnter = React.useCallback(() => {
+    setHover(true);
+  }, []);
+  const mouseLeave = React.useCallback(() => {
+    setHover(false);
+  }, []);
+
+  return (
+    <ListItem
+      click={onRowClick}
+      mouseEnter={mouseEnter}
+      mouseLeave={mouseLeave}
+    >
+      <HoverTile
+        hover={hover}
+        grpId={event.CourseDeck.deckTileId ?? DEFAULT_TILE}
+      />
+      <Column class="list_item_left">
+        <FlexTop innerClass="list_deck_name">{eventName}</FlexTop>
+        <FlexBottom>
+          <ManaCost class="mana_s20" colors={eventColors} />
+        </FlexBottom>
+      </Column>
+
+      <DraftRares event={event} />
+
+      <Column class="list_item_right">
+        <FlexTop
+          innerClass={
+            event.eventState == "Completed"
+              ? "list_event_phase"
+              : "list_event_phase_red"
+          }
+        >
+          {event.eventState}
+        </FlexTop>
+        <FlexBottom innerClass="list_match_time">
+          <relative-time datetime={new Date(event.date).toISOString()}>
+            {event.date.toString()}
+          </relative-time>
+        </FlexBottom>
+      </Column>
+
+      <Column class="list_match_result">
+        <div
+          className={getEventWinLossClass({
+            CurrentWins: event.wins,
+            CurrentLosses: event.losses
+          })}
+        >
+          {event.wins}:{event.losses}
+        </div>
+      </Column>
+
+      <ArchiveButton
+        archiveCallback={toggleArchived}
+        dataId={event.id ?? ""}
+        hover={hover}
+        isArchived={event.archived ?? false}
+      />
+    </ListItem>
+  );
+}
+
+function EventSubRows({
+  event,
+  expanded
+}: {
+  event: EventTableData;
+  expanded: boolean;
+}): JSX.Element {
+  const draftId = event.id + "-draft";
+  const dispatcher = useDispatch();
 
   const openMatch = React.useCallback(
-    (match: any): void => {
+    (match: InternalMatch): void => {
       uxMove(-100);
       dispatchAction(
         dispatcher,
@@ -104,92 +173,61 @@ export function ListItemEvent({
     [dispatcher]
   );
 
+  const matchRows: InternalMatch[] = React.useMemo(() => {
+    if (!expanded) {
+      return [];
+    }
+    const matchRows = event.stats.matchIds
+      .map(pd.match)
+      .filter(match => match !== undefined) as InternalMatch[];
+    matchRows.sort((a, b) => {
+      if (!a || !b) return 0;
+      return compareDesc(new Date(a.date), new Date(b.date));
+    });
+    if (pd.draftExists(draftId)) {
+      matchRows.unshift(pd.draft(draftId));
+    }
+    return matchRows;
+  }, [draftId, event.stats.matchIds, expanded]);
+
+  const style = expanded ? { height: matchRows.length * 64 + "px" } : {};
+
+  return (
+    <div style={style} className="list_event_expand">
+      {matchRows.map(match => {
+        return match.type == "match" ? (
+          <ListItemMatch
+            key={match.id}
+            tags={match.tags}
+            match={match}
+            openMatchCallback={openMatch}
+          />
+        ) : (
+          <ListItemDraft
+            key={match.id}
+            draft={match}
+            openDraftCallback={openDraft}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export function ListItemEvent({
+  row
+}: TableViewRowProps<EventTableData>): JSX.Element {
+  const event = row.original;
+  const [expanded, setExpanded] = React.useState(false);
+  const onRowClick = React.useCallback((): void => {
+    setExpanded(!expanded);
+  }, [expanded]);
+  const mainRowProps = { event, onRowClick };
+  const subRowsProps = { event, expanded };
   return (
     <>
-      <ListItem
-        click={onRowClick}
-        mouseEnter={mouseEnter}
-        mouseLeave={mouseLeave}
-      >
-        <HoverTile
-          hover={hover}
-          grpId={event.CourseDeck.deckTileId || DEFAULT_TILE}
-        />
-        <Column class="list_item_left">
-          <FlexTop innerClass="list_deck_name">
-            {db.events[event.InternalEventName] ||
-              event.displayName ||
-              event.InternalEventName ||
-              "Unknown"}
-          </FlexTop>
-          <FlexBottom>
-            <ManaCost class="mana_s20" colors={event.colors || []} />
-          </FlexBottom>
-        </Column>
-
-        <div
-          style={{ flexGrow: 1, display: "flex", justifyContent: "center" }}
-          className="list_item_center"
-        >
-          {draftRares}
-        </div>
-
-        <Column class="list_item_right">
-          <FlexTop
-            innerClass={
-              event.eventState == "Completed"
-                ? "list_event_phase"
-                : "list_event_phase_red"
-            }
-          >
-            {event.eventState}
-          </FlexTop>
-          <FlexBottom innerClass="list_match_time">
-            <relative-time datetime={new Date(event.date).toISOString()}>
-              {event.date.toString()}
-            </relative-time>
-          </FlexBottom>
-        </Column>
-
-        <Column class="list_match_result">
-          <div
-            className={getEventWinLossClass({
-              CurrentWins: event.wins,
-              CurrentLosses: event.losses
-            })}
-          >
-            {event.wins}:{event.losses}
-          </div>
-        </Column>
-
-        <ArchiveButton
-          archiveCallback={toggleArchived}
-          dataId={event.id || ""}
-          hover={hover}
-          isArchived={event.archived || false}
-        />
-      </ListItem>
-      <div
-        style={expanded ? { height: matchRows.length * 64 + "px" } : {}}
-        className="list_event_expand"
-      >
-        {matchRows.map(match => {
-          return match.type == "match" ? (
-            <ListItemMatch
-              key={match.id}
-              tags={match.tags}
-              match={match}
-              openMatchCallback={openMatch}
-            />
-          ) : (
-            <ListItemDraft
-              key={match.id}
-              draft={match}
-              openDraftCallback={openDraft}
-            />
-          );
-        })}
-      </div>
+      <EventMainRow {...mainRowProps} />
+      <EventSubRows {...subRowsProps} />
     </>
   );
 }
