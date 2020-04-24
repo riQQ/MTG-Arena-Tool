11b1a3ab6a95b8a6a04b1c2a5b1eebb2e8d33a39
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-04-24 12:09:19 -0300

    isEpochTimestamp fix (#1024)
    * isEpochTimestamp fix
    
    * Update util.ts
    
    * test for NaN, single liner
    
    * use *=

--- src/shared/util.ts
+++ src/shared/util.ts
@@ -559,3 +559,11 @@ export function prettierDeckData(deckData: InternalDeck): InternalDeck {
   }
   return deckData;
 }
+
+// Im not sure if this would be better as a converter instead of a check
+export function isEpochTimestamp(timestamp: number): boolean {
+  const asDate = new Date(timestamp);
+  // Even if we have a date in epoch between 2000 and
+  // 2100 it will be parsed as 1970.
+  return asDate && asDate == asDate && asDate.getUTCFullYear() < 2000;
+}
