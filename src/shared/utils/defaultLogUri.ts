7514ab1f71bb12bb0a93d8cac54bb1909b210fe5
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-06-25 18:38:00 -0300

    fix default path for Mac

--- src/shared/utils/defaultLogUri.ts
+++ src/shared/utils/defaultLogUri.ts
@@ -1,11 +1,10 @@
 export default function defaultLogUri(): string {
-  if (process.platform !== "win32") {
-    return (
-      process.env.HOME +
-      "/.wine/drive_c/user/" +
-      process.env.USER +
-      "/AppData/LocalLow/Wizards of the Coast/MTGA/Player.log"
-    );
+  if (process.platform == "darwin") {
+    // Untested! default path for logs in MacOs.
+    // return `${process.env.HOME}/Users/${process.env.USER}/Library/Logs/Wizards Of The Coast/MTGA/Player.log`;
+    return `${process.env.HOME}/Library/Logs/Wizards Of The Coast/MTGA/Player.log`;
+  } else if (process.platform == "linux") {
+    return `${process.env.HOME}/.wine/drive_c/user/${process.env.USER}/AppData/LocalLow/Wizards of the Coast/MTGA/Player.log`;
   }
 
   const windowsMtgaLogFolder =
