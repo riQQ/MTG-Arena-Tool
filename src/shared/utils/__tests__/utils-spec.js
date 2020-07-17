2db9adff01a723e3479687e086ffad0a48daa6f5
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-07-17 01:40:40 -0300

    test card types

--- src/shared/utils/__tests__/utils-spec.js
+++ src/shared/utils/__tests__/utils-spec.js
@@ -1,6 +1,8 @@
 /* eslint-env jest */
 
 import getSetCodeInEventId from "../getSetInEventId";
+import database from "../../database";
+import { cardHasType, cardType } from "../../cardTypes";
 
 describe("utils", () => {
   it("Set codes are detected preperly", () => {
@@ -11,5 +13,21 @@ describe("utils", () => {
     expect(getSetCodeInEventId("QuickDraft_GRN_20190412")).toBe("GRN");
     expect(getSetCodeInEventId("PremierDraft_IKO_20200416")).toBe("IKO");
     expect(getSetCodeInEventId("CompDraft_WAR_20190425")).toBe("WAR");
+  });
+
+  it("Card types are detected preperly", () => {
+    const phSwamp = database.card(72578);
+    if (phSwamp) {
+      expect(cardType(phSwamp)).toBe("Basic Land");
+      expect(cardHasType(phSwamp, "basic land")).toBeTruthy();
+      expect(cardHasType(phSwamp, "Basic Land")).toBeTruthy();
+      expect(cardHasType(phSwamp, "Land")).toBeTruthy();
+    }
+    const ornithopter = database.card(32951);
+    if (ornithopter) {
+      expect(cardType(ornithopter)).toBe("Creature");
+      expect(cardHasType(ornithopter, "Artifact")).toBeTruthy();
+      expect(cardHasType(ornithopter, "Creature")).toBeTruthy();
+    }
   });
 });
