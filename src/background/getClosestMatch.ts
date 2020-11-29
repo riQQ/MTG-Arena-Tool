72dc93be81426efbb145ac662e23f69cf725b8d3
eyeq <eyeq800@gmail.com>
2020-11-30 08:11:22 +0900

    Update getClosestMatch.ts filter limited/brawl (#1202)
    Co-authored-by: Manuel Etchegaray <manuel.etchegaray7@gmail.com>

--- src/background/getClosestMatch.ts
+++ src/background/getClosestMatch.ts
@@ -2,9 +2,25 @@ import db from "../shared/database-wrapper";
 import { InternalDeck, InternalMatch } from "mtgatool-shared";
 import globalStore from "../shared/store";
 
+function isLimited(format: string): boolean {
+  if (!format) {
+    return false;
+  }
+  return format.indexOf("Draft") !== -1 || format.indexOf("Sealed") !== -1;
+}
+
+function isBrawl(format: string): boolean {
+  if (!format) {
+    return false;
+  }
+  return format.indexOf("Brawl") !== -1;
+}
+
 export default function getClosestMatch(
   oppDeck: InternalDeck
 ): InternalMatch | undefined {
+  const limited = isLimited(oppDeck.format);
+  const brawl = isBrawl(oppDeck.format);
   const cards = oppDeck.mainDeck
     .map((d) => {
       const cardData = db.card(d.id);
@@ -17,6 +33,14 @@ export default function getClosestMatch(
   let date = "";
   for (const i in globalStore.matches) {
     const match = globalStore.matches[i];
+
+    if (limited && !isLimited(match.oppDeck.format)) {
+      continue;
+    }
+    if (brawl && !isBrawl(match.oppDeck.format)) {
+      continue;
+    }
+
     const matchCards = match.oppDeck.mainDeck.map((d) => {
       const cardData = db.card(d.id);
       return cardData?.name;
